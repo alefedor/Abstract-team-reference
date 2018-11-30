9b5fcb52eb3ba6d74630caa67582a2da8dcf9737
Alexandr Fedorov <fedorov2602@mail.ru>
2018-11-30 23:30:07 +0300

    Ya kek

--- code/sk.cpp
+++ code/sk.cpp
@@ -1,6 +1,12 @@
 /** Begin fast allocation */
 const int MAX_MEM = 5e8;
 int mpos = 0;
+char mem[MAX_MEM];
+inline void * operator new ( size_t n ) {
+    assert((mpos += n) <= MAX_MEM);
+    return (void *)(mem + mpos - n);
+}
+inline void operator delete ( void * ) noexcept { } // must have!
 
 /** End fast allocation */
 
