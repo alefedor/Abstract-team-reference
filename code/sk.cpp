e351c5c0617171e1f1f0828f5d3e4c31d761a7b9
Alexandr Fedorov <fedorov2602@mail.ru>
2018-11-30 23:06:14 +0300

    sk

---
+++ code/sk.cpp
@@ -0,0 +1,110 @@
+/** Begin fast allocation */
+const int MAX_MEM = 5e8;
+int mpos = 0;
+
+/** End fast allocation */
+
+
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define ll long long
+#define forn(i, n) for (int i = 0; i < (int) (n); i++)
+#define forlr(i, l, r) for (int i = (int) l; i <= (int) (r); i++)
+#define forrl(i, r, l) for (int i = (int) r; i >= (int) (l); i--)
+
+/** Interface */
+
+inline int readChar();
+template <class T = int> inline T readInt();
+template <class T> inline void writeInt( T x, char end = 0 );
+inline void writeChar( int x );
+inline void writeWord( const char *s );
+
+/** Read */
+
+static const int buf_size = 2048;
+
+inline int getChar() {
+    static char buf[buf_size];
+    static int len = 0, pos = 0;
+    if (pos == len)
+        pos = 0, len = fread(buf, 1, buf_size, stdin);
+    if (pos == len)
+        return -1;
+    return buf[pos++];
+}
+
+inline int readWord(char * buffer) {
+    int c = getChar();
+    while (c <= 32) {
+        c = getChar();
+    }
+
+    int len = 0;
+    while (c > 32) {
+        *buffer = (char) c;
+        c = getChar();
+        buffer++;
+        len++;
+    }
+    return len;
+}
+
+inline int readChar() {
+    int c = getChar();
+    while (c <= 32)
+        c = getChar();
+    return c;
+}
+
+template <class T>
+inline T readInt() {
+    int s = 1, c = readChar();
+    T x = 0;
+    if (c == '-')
+        s = -1, c = getChar();
+    while ('0' <= c && c <= '9')
+        x = x * 10 + c - '0', c = getChar();
+    return s == 1 ? x : -x;
+}
+
+/** Write */
+
+static int write_pos = 0;
+static char write_buf[buf_size];
+
+inline void writeChar( int x ) {
+    if (write_pos == buf_size)
+        fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
+    write_buf[write_pos++] = x;
+}
+
+template <class T>
+inline void writeInt( T x, char end ) {
+    if (x < 0)
+        writeChar('-'), x = -x;
+
+    char s[24];
+    int n = 0;
+    while (x || !n)
+        s[n++] = '0' + x % 10, x /= 10;
+    while (n--)
+        writeChar(s[n]);
+    if (end)
+        writeChar(end);
+}
+
+inline void writeWord( const char *s ) {
+    while (*s)
+        writeChar(*s++);
+}
+
+struct Flusher {
+    ~Flusher() {
+        if (write_pos)
+            fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
+    }
+} flusher;
+
