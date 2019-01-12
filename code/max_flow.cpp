ead6c36ed04c591490059fc3a1c0f5bec10294fa
Alexandr Fedorov <fedorov2602@mail.ru>
2019-01-12 23:37:15 +0300

    Min circulation

--- code/max_flow.cpp
+++ code/max_flow.cpp
@@ -14,12 +14,12 @@ vector<int> gr[max_n];
 int s, t;
 vector<int> d(max_n);
 
-bool bfs(int k){
+bool bfs(int k) {
     queue<int> q;
     q.push(s);
     fill(d.begin(), d.end(), -1);
     d[s] = 0;
-    while (!q.empty()){
+    while (!q.empty()) {
         int v = q.front();
         q.pop();
         for (int x : gr[v])
@@ -32,12 +32,12 @@ bool bfs(int k){
     return (d[t] != -1);
 }
 
-int dfs(int v, int flow, int k){
+int dfs(int v, int flow, int k) {
     if (flow < (1 << k))
         return 0;
     if (v == t)
         return flow;
-    for (; it[v] < gr[v].size(); it[v]++){
+    for (; it[v] < gr[v].size(); it[v]++) {
         if (d[v] + 1 != d[eds[gr[v][it[v]]].to])
             continue;
         int num = gr[v][it[v]];
@@ -51,7 +51,7 @@ int dfs(int v, int flow, int k){
     return 0;
 }
 
-void add(int fr, int to, int c, int nm){
+void add(int fr, int to, int c, int nm) {
     gr[fr].push_back(num);
     eds[num++] = edge(fr, to, c, nm);
     gr[to].push_back(num);
@@ -60,7 +60,7 @@ void add(int fr, int to, int c, int nm){
 
 int ans = 0;
     for (int k = 30; k >= 0; k--)
-        while (bfs(k)){
+        while (bfs(k)) {
             memset(it, 0, sizeof(it));
             while (int res = dfs(s, 1e9 + 500, k))
                 ans += res;
@@ -73,18 +73,18 @@ int path_num = 0;
 vector<int> paths[550];
 int flows[550];
 
-int decomp(int v, int flow){
+int decomp(int v, int flow) {
     if (flow < 1)
         return 0;
-    if (v == t){
+    if (v == t) {
         path_num++;
         flows[path_num - 1] = flow;
         return flow;
     }
-    for (int i = 0; i < gr[v].size(); i++){
+    for (int i = 0; i < gr[v].size(); i++) {
         int num = gr[v][i];
         int res = decomp(eds[num].to, min(flow, eds[num].f));
-        if (res){
+        if (res)  {
             eds[num].f -= res;
             paths[path_num - 1].push_back(eds[num].num);
             return res;
