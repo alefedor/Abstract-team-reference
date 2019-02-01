5ccae5958cf394469f2ee1e9f16a501a39f068a3
AntonYermilov <anton.yermilov@gmail.com>
2019-02-01 14:24:46 +0300

    added mobius and euler functions

--- code/sieve.cpp
+++ code/sieve.cpp
@@ -1,12 +1,16 @@
-const int N = 10000000;
-int lp[N+1];
-vector<int> pr;
+const int N = 1000000;
+
+int pr[N + 1], sz = 0;
+/* minimal prime, mobius function, euler function */
+int lp[N + 1], mu[N + 1], phi[N + 1];
  
-for (int i=2; i<=N; ++i) {
-	if (lp[i] == 0) {
-		lp[i] = i;
-		pr.push_back (i);
-	}
-	for (int j=0; j<(int)pr.size() && pr[j]<=lp[i] && i*pr[j]<=N; ++j)
+lp[1] = mu[1] = phi[1] = 1;
+for (int i = 2; i <= N; ++i) {
+	if (lp[i] == 0)
+		lp[i] = pr[sz++] = i;
+	for (int j = 0; j < sz && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
 		lp[i * pr[j]] = pr[j];
+
+    mu[i] = lp[i] == lp[i / lp[i]] ? 0 : -1 * mu[i / lp[i]];
+    phi[i] = phi[i / lp[i]] * (lp[i] == lp[i / lp[i]] ? lp[i] : lp[i] - 1);
 }
