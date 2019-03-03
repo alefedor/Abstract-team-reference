510f21fd2d0546db3c4437f312412c270af97c20
Alexandr Fedorov <fedorov2602@mail.ru>
2019-03-03 13:26:03 +0300

    Refactored fast gauss

--- code/gauss.cpp
+++ code/gauss.cpp
@@ -1,45 +1,22 @@
-const ll MOD = ...;
-vector<pair<int, int>> rows[MAXN];  // a[i][rows[i][j].first]=rows[i][j].second; b[i]=a[i][n]
-vector<ll> res;
-int n;
+// fast gauss begins
 
-int a[MAXN];
-int toZero[MAXN];
-int zro[MAXN];
-int p[MAXN];
-int invP[MAXN];
+using elem_t = int;
+// a[i][rows[i][j].first]=rows[i][j].second; b[i]=a[i][n]
+bool gauss(vector<vector<pair<int, elem_t>>> rows, vector<elem_t> &res) {
+    int n = rows.size();
 
-void subtract(int i, int j, int c) {
-    for (auto el: rows[j]) {
-        if (a[el.first] == 0) {
-            rows[i].push_back({el.first, 0});
-        }
-        a[el.first] = (a[el.first] + MOD - (c * 1ll * el.second) % MOD) % MOD;
-    }
-    rows[i].erase(
-            std::remove_if(rows[i].begin(), rows[i].end(),
-                           [](const pair<int, int>& p) { return a[p.first] == 0; }),
-            rows[i].end());
-}
+    res.resize(n + 1, 0);
+    vector<int> p(n + 1);
+    iota(p.begin(), p.end(), 0);
+    vector<int> toZero(n + 1, -1);
+    vector<int> zro(n + 1);
+    vector<elem_t> a(n + 1);
 
-void mul(int i, int c) {
-    for (auto& el: rows[i]) {
-        el.second = (a[el.first] * 1ll * c) % MOD;
-        a[el.first] = 0;
-    }
-    rows[i].erase(
-            std::remove_if(rows[i].begin(), rows[i].end(),
-                           [](const pair<int, int>& p) { return p.second == 0; }),
-            rows[i].end());
-}
+    // optional: sort rows
 
-void sortRows() {
-    for (int i = 0; i <= n; i++) {
-        p[i] = i;
-        toZero[i] = -1;
-    }
-    sort(p, p + n, [](int i, int j) { return rows[i].size() < rows[j].size(); });
-    vector<vector<pair<int, int>>> rs(n);
+    sort(p.begin(), p.begin() + n, [&rows](int i, int j) { return rows[i].size() < rows[j].size(); });
+    vector<int> invP(n + 1);
+    vector<vector<pair<int, elem_t>>> rs(n);
     for (int i = 0; i < n; i++) {
         invP[p[i]] = i;
         rs[i] = rows[p[i]];
@@ -52,52 +29,70 @@ void sortRows() {
             }
         }
     }
-}
 
-void gauss() {
-    sortRows();
+
     for (int i = 0; i < n; i++) {
         for (auto& el: rows[i]) {
             a[el.first] = el.second;
         }
-        while (1) {
+        while (true) {
             int k = -1;
             for (auto& el: rows[i]) {
-                if (a[el.first] != 0 && toZero[el.first] != -1 &&
+                if (!isZero(a[el.first]) && toZero[el.first] != -1 &&
                     (k == -1 || toZero[el.first] < toZero[k])) {
                     k = el.first;
                 }
             }
-            if (k == -1) {
+            if (k == -1)
                 break;
+
+            int j = toZero[k];
+            elem_t c = a[k];
+
+            for (auto el: rows[j]) {
+                if (isZero(a[el.first]))
+                    rows[i].emplace_back(el.first, 0);
+                a[el.first] = sub(a[el.first], mult(c, el.second));
             }
-            subtract(i, toZero[k], a[k]);
+
+            auto cond = [&a](const pair<int, elem_t>& p) { return isZero(a[p.first]); };
+
+            rows[i].erase(std::remove_if(rows[i].begin(), rows[i].end(), cond), rows[i].end());
         }
+
         bool ok = false;
         for (auto& el: rows[i]) {
-            if (el.first < n && a[el.first] != 0) {
+            if (el.first < n && !isZero(a[el.first])) {
                 toZero[el.first] = i;
                 zro[i] = el.first;
-//                det = (det * a[el.first]) % MOD;
-                mul(i, divM(1, a[el.first]));
+//              det = (det * a[el.first]) % MOD;
+
+                elem_t c = divM(1, a[el.first]);
+                for (auto& el: rows[i]) {
+                    el.second = mult(a[el.first], c);
+                    a[el.first] = 0;
+                }
+
                 ok = true;
                 break;
             }
         }
+
         if (!ok) {
-            res[0] = -1;
-//            det = 0;
-            return;
+//          det = 0;
+            return false;
         }
     }
-    res[n] = MOD - 1;
+
+    res[n] = sub(0, 1);
     for (int i = n - 1; i >= 0; i--) {
         int k = zro[i];
-        for (auto& el : rows[i]) {
-            if (el.first != k) {
-                res[p[k]] += (MOD - (el.second * res[p[el.first]]) % MOD);
-                res[p[k]] %= MOD;
-            }
-        }
+        for (auto& el : rows[i])
+            if (el.first != k)
+                res[p[k]] = sub(res[p[k]], mult(el.second, res[p[el.first]]));
     }
+
+    return true;
 }
+
+// fast gauss ends
