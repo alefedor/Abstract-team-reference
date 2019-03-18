1a86a01ad311034abb8affc7b209d1cebf7385cc
Alexandr Fedorov <fedorov2602@mail.ru>
2019-03-18 12:19:20 +0300

    Added convex hull trick

--- code/convex_hull_trick.cpp
+++ code/convex_hull_trick.cpp
@@ -1,75 +1,93 @@
-struct line {
-    ll k, b;
-    ll at(ll x) const {
-        return k * x + b;
-    }
-};
- double intersec(line a, line b) {
-    return 1.0 * (b.b - a.b) / (a.k - b.k);
+// convex hull trick begins
+
+typedef long long ftype;
+typedef complex<ftype> point;
+#define x real
+#define y imag
+
+ftype dot(point const& a, point const& b) {
+    return (conj(a) * b).x();
 }
- struct convex_hull_trick {
-    vector<double> x = {-1e18};
-    vector<line> lines;
-     void add(line l) {
-        // l.k increasing
-        if (lines.empty()) {
-            lines.pb(l);
+
+ftype f(point const& a, int x) {
+    return dot(a, {compressed[x], 1});
+    //return dot(a, {x, 1});
+}
+
+int pos = 0;
+
+// (x, y) -> (k, b) -> kb + x
+struct li_chao { // for min
+    vector<point> line;
+
+    li_chao(int maxn) {
+        line.resize(4 * maxn, {0, inf});
+    }
+
+    void add_line(int v, int l, int r, int a, int b, point nw) {
+        if (r <= a || b <= l) return; // remove if no [a, b) query
+
+        int m = (l + r) >> 1;
+
+        if (!(a <= l && r <= b)) { // remove if no [a, b) query
+            add_line(v + v + 1, l, m, a, b, nw);
+            add_line(v + v + 2, m, r, a, b, nw);
+            return;
+        }
+
+        bool lef = f(nw, l) < f(line[v], l);
+        bool mid = f(nw, m) < f(line[v], m);
+
+        if (mid) swap(line[v], nw);
+
+        if (l == r - 1)
+            return;
+
+        if (lef != mid)
+            add_line(v + v + 1, l, m, a, b, nw);
+        else
+            add_line(v + v + 2, m, r, a, b, nw);
+    }
+
+    ftype get(int v, int l, int r, int x) {
+        if(l == r - 1)
+            return f(line[v], x);
+        int m = (l + r) / 2;
+        if(x < m) {
+            return min(f(line[v], x), get(v + v + 1, l, m, x));
         } else {
-            while (lines.size() > 1 && intersec(l, lines[lines.size() - 2]) < x.back()) {
-                lines.pop_back();
-                x.pop_back();
-            }
-            x.push_back(intersec(l, lines.back()));
-            lines.push_back(l);
+            return min(f(line[v], x), get(v + v + 2, m, r, x));
         }
     }
-};
- struct cht_forward_iterator {
-    int ci = 0;
-     ll promote(const convex_hull_trick& cht, ll value) {
-        while (ci < (int)cht.x.size() - 1 && cht.x[ci + 1] < value)
-            ci++;
-        return cht.lines[ci].at(value);
+
+} cdt(maxn);
+
+// convex hull with stack
+
+ftype cross(point a, point b) {
+    return (conj(a) * b).y();
+}
+
+vector<point> hull, vecs;
+
+void add_line(ftype k, ftype b) {
+    point nw = {k, b};
+    while(!vecs.empty() && dot(vecs.back(), nw - hull.back()) < 0) {
+        hull.pop_back();
+        vecs.pop_back();
     }
-};
- const int g = 275;
- struct sqrt_dec {
-    struct block {
-        vector<int> bs;
-        convex_hull_trick cht;
-        cht_forward_iterator it;
-         block(vector<int> b) {
-            bs = b;
-            int k = b.size();
-            forn(i, k) {
-                cht.add({ b[i], 1ll * b[i] * (k - i) });
-            }
-        }
-         ll value(ll x) {
-            if (cht.lines.empty())
-                return 0;
-            return it.promote(cht, x);
-        }
-    };
-     void insert(block& bl, int b) {
-        auto v = std::move(bl.bs);
-        v.insert(lower_bound(all(v), b), b);
-        bl = block(std::move(v));
+    if(!hull.empty()) {
+        vecs.push_back(1i * (nw - hull.back()));
     }
-     vector<block> blocks;
-     sqrt_dec(int n) {
-        blocks.resize((n + g - 1) / g, block({}));
-    }
-     void add(int i, int b) {
-        insert(blocks[i / g], b);
-    }
-     ll get_max() {
-        int sm = 0;
-        ll ans = 0;
-        for (int i = blocks.size() - 1; i >= 0; i--) {
-            ans = max(ans, blocks[i].value(sm));
-            sm += blocks[i].bs.size();
-        }
-        return ans;
-    }
-};
+    hull.push_back(nw);
+}
+
+int get(ftype x) {
+    point query = {x, 1};
+    auto it = lower_bound(vecs.begin(), vecs.end(), query, [](point a, point b) {
+        return cross(a, b) > 0;
+    });
+    return dot(query, hull[it - vecs.begin()]);
+}
+
+// honvex hull trick ends
